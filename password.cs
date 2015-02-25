77475b12def809448a4cd7b0417c9f82f2ab6e57
pancake-mania <hiroto.koshiyama@gmail.com>
2015-02-25 15:48:14 +0900

    password.cs add
    easy password Encryption

---
+++ password.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//XORによる簡易的な暗号化と複合化
+class password
+{
+	static void Main() {
+
+		string str = "abcdefghijklmnopqrstuvwxyzABC!#$%&'()=~|";
+		char[] c = str.ToCharArray();
+		Console.WriteLine(str);
+
+		//暗号化
+		for (int i = 0; i < str.Length; i++ )
+		{
+			c[i]=(char)((int)c[i] ^ 1);
+		}
+
+		string str2= new string(c);
+		Console.WriteLine(str2);
+
+		c = str2.ToCharArray();
+		//複合化
+		for (int i = 0; i < str2.Length; i++)
+		{
+			c[i] = (char)((int)c[i] ^ 1);
+		}
+
+		string str3 = new string(c);
+		Console.WriteLine(str3);
+
+	}
+}
